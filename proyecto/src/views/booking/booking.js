9a96fccb684f351f5fd3090c45948bf70a19a273
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-02-16 11:43:44 -0600

    Router

---
+++ proyecto/src/views/booking/booking.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./booking.css";
+
+function Booking() {
+    return (
+        <div className="Booking">
+            <h1>¡Hola, mundo!</h1>
+            <h1>Soy booking</h1>
+        </div>
+    );
+}
+
+export default Booking;
