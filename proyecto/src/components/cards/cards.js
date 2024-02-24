1724e053e81f93b8e1dd4e12ffdca12300b2bb3e
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-02-23 20:07:12 -0600

    Inicio de llamadas al api

--- proyecto/src/components/cards/cards.js
+++ proyecto/src/components/cards/cards.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
 import Accordion from 'react-bootstrap/Accordion';
@@ -15,6 +15,16 @@ import './cards.css';
 import { Grid } from '@mui/material';
 
 function MyVerticallyCenteredModal(props) {
+    const [menu, setMenu] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5050/plates?id_restaurante=" + props.id)
+            .then((response) => response.json())
+            .then((data) => setMenu(data));
+    }, [props.id]);
+
+    const ratingValue = props.rating;
+
     return (
         <Modal
             {...props}
@@ -26,46 +36,40 @@ function MyVerticallyCenteredModal(props) {
                 <Grid item xs={12} sm={5}>
                     <img
                         src="https://bde04f90fcbcc7b99af9-a4cf3e88ec567f5b6c6819f1d482f77f.ssl.cf1.rackcdn.com/16_773_r_0.jpg?v=1752"
-                        alt="Tre Fratelli"
+                        alt={props.name}
                         style={{ width: "100%", height: "100%", objectFit: "cover" }}
                     />
                 </Grid>
                 <Grid item xs={12} sm={7}>
                     <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title-vcenter">
-                            <h1>Tre Fratelli</h1>
+                            <h1>{props.name}</h1>
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <h4>Informacion</h4>
                         <p>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                            consectetur ac, vestibulum at eros.
+                            {props.des}
                         </p>
                         <h4>Menu</h4>
-                        <div className='menu' style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-                            <Card>
-                                <Card.Body>
-                                    Comida1
-                                </Card.Body>
-                            </Card>
-                            <Card>
-                                <Card.Body>
-                                    Comida2
-                                </Card.Body>
-                            </Card>
-                            <Card>
-                                <Card.Body>
-                                    Comida3
-                                </Card.Body>
-                            </Card>
-                        </div>
+                        {menu.length > 0 ? (
+                            <div className='menu' style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+                                {menu.map((platillo) => (
+                                    <Card key={platillo._id}>
+                                        <Card.Body>
+                                            {platillo.nombre}
+                                        </Card.Body>
+                                    </Card>
+                                ))}
+                            </div>
+                        ) : (
+                            <p>Menú no disponible...</p>
+                        )}
                         <br></br>
                         <h4>Rating</h4>
                         <Rating
                             name="customized-empty"
-                            value={4.5}
+                            value={ratingValue}
                             readOnly
                             precision={0.5}
                             emptyIcon={<StarBorderIcon fontSize="inherit" />}
@@ -167,7 +171,7 @@ function MyVerticallyCenteredModal(props) {
     );
 }
 
-function Cards() {
+function Cards(props) {
     const [isHovered, setIsHovered] = useState(false);
     const [modalShow, setModalShow] = useState(false);
 
@@ -182,7 +186,7 @@ function Cards() {
     return (
         <>
             <Card
-                style={{ maxWidth: "450px", transform: isHovered ? 'scale(1.02)' : 'scale(1)', transition: 'transform 0.3 ease', cursor: 'pointer', background: "black", color: "#EAE0D5"}}
+                style={{ maxWidth: "450px", transform: isHovered ? 'scale(1.02)' : 'scale(1)', transition: 'transform 0.3 ease', cursor: 'pointer', background: "black", color: "#EAE0D5" }}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 onClick={() => setModalShow(true)}
@@ -190,13 +194,18 @@ function Cards() {
                 <Card.Img variant="top" src="https://www.guatemala.com/fotos/2021/01/Nuevas-promociones-2x1-de-Trefratelli-en-la-app-de-Cupones-Guatemala.com-1-885x500.jpg" />
                 <Card.Body>
                     <Card.Title style={{ textAlign: "center" }}>
-                        <h2>Tre Fratelli</h2>
+                        <h2>{props.name}</h2>
                     </Card.Title>
                 </Card.Body>
             </Card>
             <MyVerticallyCenteredModal
                 show={modalShow}
                 onHide={() => setModalShow(false)}
+                name={props.name}
+                image={props.image}
+                des={props.desc}
+                rating={props.rating}
+                id={props.id}
             />
         </>
     );
